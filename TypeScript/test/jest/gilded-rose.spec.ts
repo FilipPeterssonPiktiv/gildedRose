071d97d665f3d3caaa6d3b8daae8144d3ad0f151
Fredrik Andersson <fredrik@piktiv.se>
2022-05-24 14:45:35 +0200

    add tests for legendary items

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,40 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+  it('should contain items', () => {
+    const expectedItems = [
+      new Item('foo', 0, 0),
+      new Item('bar', 0, 0),
+    ];
+    const gildedRose = new GildedRose(expectedItems);
+
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+
+    expect(items[0].name).toBe('foo');
+    expect(items[1].name).toBe('bar');
+  });
+
+  describe('with legendary item', () => {
+    it('should not degrade quality', () => {
+      const expectedItems = [
+        new Item('Sulfuras, Hand of Ragnaros', 5, 30),
+      ];
+      const gildedRose = new GildedRose(expectedItems);
+
+      const items = gildedRose.updateQuality();
+
+      expect(items[0].quality).toBe(30);
+    });
+
+    it('should not decrease time until sell', () => {
+      const expectedItems = [
+        new Item('Sulfuras, Hand of Ragnaros', 5, 30),
+      ];
+      const gildedRose = new GildedRose(expectedItems);
+
+      const items = gildedRose.updateQuality();
+
+      expect(items[0].sellIn).toBe(5);
+    });
   });
 });
